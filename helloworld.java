eed0440da7446c862e7130147b93dfb3401c6920
adibhatt <94904653+adibhatt@users.noreply.github.com>
2024-02-06 14:06:07 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class helloworld {
+    public static void main(String args[])
+    {
+        Scanner sc   = new Scanner(System.in);
+        
+        System.out.println("Enter the number");
+        int x = sc.nextInt();
+
+        System.out.println("Hello world" + x);
+    }
+}
